9a676ff3598b31d5d10262f43ce1c570138c7674
Nitish Pandey <nitishpandey.work@gmail.com>
2024-11-28 11:26:46 +0545

    initial commit

---
+++ src/routes.tsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter, RouteObject } from "react-router-dom";
+import App from "./App";
+import AuthLayout from "./layouts/AuthLayout";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+
+export const routes: RouteObject[] = [
+    {
+        element: <App />,
+        path: "/",
+        children: [
+            {
+                path: "auth",
+                element: <AuthLayout />,
+                children: [
+                    {
+                        path: "login",
+                        element: <Login />,
+                    },
+                    {
+                        path: "register",
+                        element: <Register />,
+                    },
+                ],
+            },
+        ],
+    },
+];
+
+export const router = createBrowserRouter(routes);
